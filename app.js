b413d2874ddd37408d75ac8421993f128135866c
BYUNGSOO <eedock92@naver.com>
2021-04-24 16:58:29 +0900

    logout, stories

---
+++ app.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const express = require('express')
+//const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+//const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+
+
+//Load config
+dotenv.config({ path : './config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
+
+
+connectDB()
+
+const app = express()
+
+// Logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+
+//Templating Engine
+app.engine('hbs' , exphbs({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+
+//Sessions
+app.use(session({
+    secret : 'keyboard cat',
+    resave : false,
+    saveUninitialized : false,
+//    store : new MongoStore({mongooseConnection : mongoose.connection})
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+
+const PORT = process.env.PORT || 5000
+
+
+
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
